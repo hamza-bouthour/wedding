f1e57dbaf631ec0b35534f3d86b1115f9be74e3d
hamza bouthour <61101578+hamza-bouthour@users.noreply.github.com>
2025-08-05 21:30:54 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,6 @@ export default defineConfig({
   preview: {
     port: 4173,
     host: true,
-    allowedHosts: ["wed-production-6dd9.up.railway.app"],
+    allowedHosts: ["wedding-production-6093.up.railway.app"],
   },
 });
